f5550158f351025dc593c785497a628e986c4f7b
Daniel Yoder <dan@dashkite.com>
2023-12-21 15:00:06 -0800

    housekeeping

---
+++ .genie/tasks/index.js
@@ -0,0 +1,63 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+const _promises = /*#__PURE__*/ _interop_require_default(require("node:fs/promises"));
+const _genie = /*#__PURE__*/ _interop_require_wildcard(require("@dashkite/genie"));
+function _interop_require_default(obj) {
+    return obj && obj.__esModule ? obj : {
+        default: obj
+    };
+}
+function _getRequireWildcardCache(nodeInterop) {
+    if (typeof WeakMap !== "function") return null;
+    var cacheBabelInterop = new WeakMap();
+    var cacheNodeInterop = new WeakMap();
+    return (_getRequireWildcardCache = function(nodeInterop) {
+        return nodeInterop ? cacheNodeInterop : cacheBabelInterop;
+    })(nodeInterop);
+}
+function _interop_require_wildcard(obj, nodeInterop) {
+    if (!nodeInterop && obj && obj.__esModule) {
+        return obj;
+    }
+    if (obj === null || typeof obj !== "object" && typeof obj !== "function") {
+        return {
+            default: obj
+        };
+    }
+    var cache = _getRequireWildcardCache(nodeInterop);
+    if (cache && cache.has(obj)) {
+        return cache.get(obj);
+    }
+    var newObj = {
+        __proto__: null
+    };
+    var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor;
+    for(var key in obj){
+        if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) {
+            var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null;
+            if (desc && (desc.get || desc.set)) {
+                Object.defineProperty(newObj, key, desc);
+            } else {
+                newObj[key] = obj[key];
+            }
+        }
+    }
+    newObj.default = obj;
+    if (cache) {
+        cache.set(obj, newObj);
+    }
+    return newObj;
+}
+// TODO incorporate into preset
+_genie.define("bin", async function() {
+    await _promises.default.mkdir("build/node/src/bin", {
+        recursive: true
+    });
+    return _promises.default.copyFile("src/bin/tempo", "build/node/src/bin/tempo");
+});
+_genie.after("build", "bin"); //# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiL3Rhc2tzL2luZGV4LmNvZmZlZSJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQSxPQUFPLEVBQVAsTUFBQTs7QUFDQSxPQUFPLENBQUEsU0FBUCxNQUFBLGtCQURBOzs7QUFJQSxLQUFLLENBQUMsTUFBTixDQUFhLEtBQWIsRUFBb0IsTUFBQSxRQUFBLENBQUEsQ0FBQTtFQUNsQixNQUFNLEVBQUUsQ0FBQyxLQUFILENBQVMsb0JBQVQsRUFBK0I7SUFBQSxTQUFBLEVBQVc7RUFBWCxDQUEvQjtTQUNOLEVBQUUsQ0FBQyxRQUFILENBQVksZUFBWixFQUE2QiwwQkFBN0I7QUFGa0IsQ0FBcEI7O0FBSUEsS0FBSyxDQUFDLEtBQU4sQ0FBWSxPQUFaLEVBQXFCLEtBQXJCIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IEZTIGZyb20gXCJub2RlOmZzL3Byb21pc2VzXCJcbmltcG9ydCAqIGFzIEdlbmllIGZyb20gXCJAZGFzaGtpdGUvZ2VuaWVcIlxuXG4jIFRPRE8gaW5jb3Jwb3JhdGUgaW50byBwcmVzZXRcbkdlbmllLmRlZmluZSBcImJpblwiLCAtPlxuICBhd2FpdCBGUy5ta2RpciBcImJ1aWxkL25vZGUvc3JjL2JpblwiLCByZWN1cnNpdmU6IHRydWVcbiAgRlMuY29weUZpbGUgXCJzcmMvYmluL3RlbXBvXCIsIFwiYnVpbGQvbm9kZS9zcmMvYmluL3RlbXBvXCJcblxuR2VuaWUuYWZ0ZXIgXCJidWlsZFwiLCBcImJpblwiIl19
+ //# sourceURL=/tasks/index.coffee
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi90YXNrcy9pbmRleC5jb2ZmZWUiLCI8YW5vbj4iXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IEZTIGZyb20gXCJub2RlOmZzL3Byb21pc2VzXCJcbmltcG9ydCAqIGFzIEdlbmllIGZyb20gXCJAZGFzaGtpdGUvZ2VuaWVcIlxuXG4jIFRPRE8gaW5jb3Jwb3JhdGUgaW50byBwcmVzZXRcbkdlbmllLmRlZmluZSBcImJpblwiLCAtPlxuICBhd2FpdCBGUy5ta2RpciBcImJ1aWxkL25vZGUvc3JjL2JpblwiLCByZWN1cnNpdmU6IHRydWVcbiAgRlMuY29weUZpbGUgXCJzcmMvYmluL3RlbXBvXCIsIFwiYnVpbGQvbm9kZS9zcmMvYmluL3RlbXBvXCJcblxuR2VuaWUuYWZ0ZXIgXCJidWlsZFwiLCBcImJpblwiIixudWxsXSwibmFtZXMiOlsiR2VuaWUiLCJkZWZpbmUiLCJGUyIsIm1rZGlyIiwicmVjdXJzaXZlIiwiY29weUZpbGUiLCJhZnRlciJdLCJtYXBwaW5ncyI6Ijs7OztpRUFBQTsrREFDQTs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7O0FBR0FBLE9BQU1DLE1BQU4sQ0FBYSxPQUFPO0lBQ2xCLE1BQU1DLGlCQUFFLENBQUNDLEtBQUgsQ0FBUyxzQkFBc0I7UUFBQUMsV0FBVztJQUFYO1dBQ3JDRixpQkFBRSxDQUFDRyxRQUFILENBQVksaUJBQWlCO0FBRlg7QUFJcEJMLE9BQU1NLEtBQU4sQ0FBWSxTQUFTIn0=
--- tasks/index.coffee
+++ tasks/index.coffee
@@ -1,14 +1,9 @@
-import "coffeescript/register"
-import FS from "fs/promises"
-
-import * as t from "@dashkite/genie"
-import preset from "@dashkite/genie-presets"
-
-preset t
+import FS from "node:fs/promises"
+import * as Genie from "@dashkite/genie"
 
 # TODO incorporate into preset
-t.define "bin", ->
+Genie.define "bin", ->
   await FS.mkdir "build/node/src/bin", recursive: true
   FS.copyFile "src/bin/tempo", "build/node/src/bin/tempo"
 
-t.after "build", "bin"
+Genie.after "build", "bin"
